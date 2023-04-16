7e40d8ba0e53c149ffbf5c73a2ea059403fa8e38
Filip Granö <filip-accounts@grano.me>
2023-04-16 23:01:04 +0300

    Cligpt completion implementation

---
+++ oai_tools/cligpt_completion.py
@@ -0,0 +1,27 @@
+import sys
+
+import openai
+
+from oai_tools import get_api_key, read_config
+from oai_tools.cligpt import generate_command
+
+openai.api_key = get_api_key()
+config = read_config()
+cligpt_config = config.get("cligpt", {})
+MODEL = cligpt_config.get("model", "gpt-3.5-turbo")
+MAX_TOKENS_COMMAND = cligpt_config.get("max_tokens", {}).get("command", 100)
+TEMPERATURE_COMMAND = cligpt_config.get("temperature", {}).get("command", 0.9)
+
+
+def main():
+    try:
+        prompt = " ".join(sys.argv[1:])
+        completion_response = generate_command(prompt)
+        completion = completion_response.choices[0].message.content.strip()
+        print(completion)
+    except KeyboardInterrupt:
+        print("\nAborted by user.")
+
+
+if __name__ == "__main__":
+    main()
