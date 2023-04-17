52143994861cd8f6c817cbc4df14d91977f240ca
Filip Granö <filip-accounts@grano.me>
2023-04-17 11:34:08 +0300

    use basename of shell for queries

--- oai_tools/cligpt.py
+++ oai_tools/cligpt.py
@@ -36,7 +36,7 @@ def get_shell() -> str:
 
 def generate_command(prompt: str) -> openai.ChatCompletion:
     """Generate a shell command using GPT-3.5 based on the given prompt."""
-    query = f"""Write a shell command that works on the {platform.system()} platform in the following shell: {get_shell()}
+    query = f"""Write a shell command that works on the {platform.system()} platform in the following shell: {os.path.basename(get_shell())}
 
         The command must accomplish this task:
 
@@ -58,7 +58,7 @@ def generate_command(prompt: str) -> openai.ChatCompletion:
 
 def explain_command(suggestion: str, prompt: str) -> openai.ChatCompletion:
     """Generate an explanation of a suggested command using GPT-3.5."""
-    query = f"Explain in brief how the command '{suggestion}' works, what it does, and if it's safe to use (why not if not). Also state if it fulfills the requested task '{prompt}' or not. Return the explanation and task fulfillment status in a single line. No other words, code highlighting, or text. Don't repeat the command or the task."
+    query = f"Explain in brief how the command '{suggestion}' works on the {platform.system()} platform in the {os.path.basename(get_shell())} shell, what it does, and if it's safe to use (why not if not). Also state if it fulfills the requested task '{prompt}' or not. Return the explanation and task fulfillment status in a single line. No other words, code highlighting, or text. Don't repeat the command or the task."
 
     logging.debug("Explaining command: %s", suggestion)
 
